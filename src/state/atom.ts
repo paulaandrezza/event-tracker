4c44f52350d5f2515f463156ed23e6633bb40023
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-29 21:20:29 -0300

    Filtrando eventos

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,22 +1,13 @@
 import { atom } from "recoil"
 import { IEvento } from "../interfaces/IEvento"
+import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos"
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listadDeEventosState",
-  default: [
-    {
-      descricao: "Estudar React",
-      inicio: new Date("2023-06-30T09:00"),
-      fim: new Date("2023-06-30T13:00"),
-      completo: false,
-      id: 1642342747,
-    },
-    {
-      descricao: "Estudar Recoil",
-      inicio: new Date("2023-06-29T09:00"),
-      fim: new Date("2023-06-29T11:00"),
-      completo: false,
-      id: 1642342959,
-    },
-  ],
+  default: [],
+})
+
+export const filtroDeEventos = atom<IFiltroDeEventos>({
+  key: "filtroDeEventos",
+  default: {},
 })
