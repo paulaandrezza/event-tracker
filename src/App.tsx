5ea15d7713fa7e370b65c39d1da5c740047a18a2
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-29 22:01:14 -0300

    Seletores Async e Depuração

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
+import { Suspense } from "react"
 import { RecoilRoot } from "recoil"
 import style from "./App.module.scss"
 import Calendario from "./components/Calendario"
 import Card from "./components/Card"
+import DebugObserver from "./components/DebugObserver"
 import Formulario from "./components/Formulario"
 import ListaDeEventos from "./components/ListaDeEventos"
 
 function App() {
   return (
     <RecoilRoot>
-      <div className={style.App}>
-        <div className={style.Coluna}>
-          <Card>
-            <Formulario />
-          </Card>
-          <hr />
-          <Card>
-            <ListaDeEventos />
-          </Card>
+      <DebugObserver />
+      <Suspense fallback="Está carregando">
+        <div className={style.App}>
+          <div className={style.Coluna}>
+            <Card>
+              <Formulario />
+            </Card>
+            <hr />
+            <Card>
+              <ListaDeEventos />
+            </Card>
+          </div>
+          <div className={style.Coluna}>
+            <Calendario />
+          </div>
         </div>
-        <div className={style.Coluna}>
-          <Calendario />
-        </div>
-      </div>
+      </Suspense>
     </RecoilRoot>
   )
 }
--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,10 +1,11 @@
 import { atom } from "recoil"
 import { IEvento } from "../interfaces/IEvento"
 import { IFiltroDeEventos } from "../interfaces/IFiltroDeEventos"
+import { eventosAsync } from "./hooks/seletores"
 
 export const listaDeEventosState = atom<IEvento[]>({
   key: "listadDeEventosState",
-  default: [],
+  default: eventosAsync,
 })
 
 export const filtroDeEventos = atom<IFiltroDeEventos>({
